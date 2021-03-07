3395da397055ceded164bf6b9e4e3a016656585a
Yuta HASHIGUCHI <9391978+ha4gu@users.noreply.github.com>
2021-03-07 19:49:49 +0900

    factoryファイルへのannotate付与

--- spec/factories/bitbucket_account_factory.rb
+++ spec/factories/bitbucket_account_factory.rb
@@ -7,3 +7,19 @@ FactoryBot.define do
     workspace { Faker::Internet.slug }
   end
 end
+
+# == Schema Information
+#
+# Table name: hosting_accounts
+#
+#  id           :integer          not null, primary key
+#  custom_name  :string
+#  organization :string
+#  password     :string
+#  token        :string
+#  type         :string           not null
+#  username     :string
+#  workspace    :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
